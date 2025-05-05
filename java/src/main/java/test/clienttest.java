f8fa620738f521625d6614105ae739d3d76c5ac4
Gregory-Usharov <gregusharov@gmail.com>
2025-05-05 10:25:19 -0400

    saving the mcprotocol approach, going to try something completelty different for now

--- java/src/main/java/test/clienttest.java
+++ java/src/main/java/test/clienttest.java
@@ -5,24 +5,22 @@ import java.net.SocketAddress;
 import java.util.UUID;
 
 import org.geysermc.mcprotocollib.auth.GameProfile;
-import org.geysermc.mcprotocollib.auth.SessionService;
 import org.geysermc.mcprotocollib.network.Session;
 import org.geysermc.mcprotocollib.network.event.server.ServerAdapter;
 import org.geysermc.mcprotocollib.network.event.server.SessionAddedEvent;
+import org.geysermc.mcprotocollib.network.event.session.SessionAdapter;
 import org.geysermc.mcprotocollib.network.factory.ClientNetworkSessionFactory;
+import org.geysermc.mcprotocollib.network.packet.Packet;
 import org.geysermc.mcprotocollib.network.server.NetworkServer;
 import org.geysermc.mcprotocollib.network.session.ClientNetworkSession;
-import org.geysermc.mcprotocollib.protocol.ClientListener;
-import org.geysermc.mcprotocollib.protocol.MinecraftConstants;
 import org.geysermc.mcprotocollib.protocol.MinecraftProtocol;
-import org.geysermc.mcprotocollib.protocol.data.handshake.HandshakeIntent;
 
 import net.lenni0451.commons.httpclient.HttpClient;
 import net.raphimc.minecraftauth.MinecraftAuth;
 import net.raphimc.minecraftauth.step.java.session.StepFullJavaSession;
 import net.raphimc.minecraftauth.step.msa.StepMsaDeviceCode;
 
-public class MinecraftClient {
+public class clienttest {
 
     public static void main(String[] args) throws Exception {
         HttpClient httpClient = MinecraftAuth.createHttpClient();
@@ -35,7 +33,7 @@ public class MinecraftClient {
             })
         );
         //the above authenticates and gets an access token
-        HandshakeIntent h = HandshakeIntent.LOGIN;
+        //HandshakeIntent h = HandshakeIntent.LOGIN;
         String name = javaSession.getMcProfile().getName();
         UUID uuid = javaSession.getMcProfile().getId();
         String accessToken = javaSession.getMcProfile().getMcToken().getAccessToken();
@@ -44,40 +42,54 @@ public class MinecraftClient {
             
         MinecraftProtocol hhe = new MinecraftProtocol(new GameProfile(uuid, name), accessToken);
         //hhe.newClientSession(session); //look into this method
-        // Create a ClientSession
-        /*ClientNetworkSession clisession = ClientNetworkSessionFactory.factory()
-                .setAddress("mc.minehut.com")
-                .setProtocol(hhe)
-                .create();
-                */
+        //Create a ClientSession
+        
+                
         //try adding the client listeener to proxyclient and if that doesnt work add it to realclient
         SocketAddress bindAddress = new InetSocketAddress("127.0.0.1", 25565); // Localhost:25565
-        NetworkServer proxy = new NetworkServer(bindAddress, ()->hhe);
+        NetworkServer proxy = new NetworkServer(bindAddress, MinecraftProtocol::new);
         ServerAdapter proxlissten = new ServerAdapter(){
             @Override
             public void sessionAdded(SessionAddedEvent event) {
-                System.out.print("session joined");
-                Session localSession = event.getSession();
-                
+                System.out.println("session joined");
+                Session localsess = event.getSession();
                 ClientNetworkSession clisession = ClientNetworkSessionFactory.factory()
                 .setAddress("mc.minehut.com")
                 .setProtocol(hhe)
                 .create();
-                clisession.setFlag(MinecraftConstants.PROFILE_KEY, new GameProfile(uuid, name));
-                clisession.setFlag(MinecraftConstants.ACCESS_TOKEN_KEY, accessToken);
-                clisession.setFlag(MinecraftConstants.AUTOMATIC_KEEP_ALIVE_MANAGEMENT, true);
-                clisession.setFlag(MinecraftConstants.FOLLOW_TRANSFERS, true);
-                clisession.setFlag(MinecraftConstants.SESSION_SERVICE_KEY, new SessionService());
-                clisession.addListener(new ClientListener(h));
+                localsess.addListener(new SessionAdapter(){
+                    @Override
+                    public void packetReceived(Session session, Packet packet) {
+                        System.out.println("Local Client");
+                        System.out.println("packet sent");
+                        System.out.println("packet type: " + packet.getClass().getSimpleName());
+                        clisession.send(packet);
+                        
+                    }
+                    @Override
+                    public void packetSent(Session session, Packet packet) {
+                        System.out.println("Local Client");
+                        System.out.println("packet recieved");
+                        System.out.println("packet type: " + packet.getClass().getSimpleName());
+                        
+                    }
+                });
+                clisession.addListener(new SessionAdapter(){
+                    @Override
+                    public void packetReceived(Session session, Packet packet) {
+                        System.out.println("Local Client");
+                        System.out.println("packet sent");
+                        System.out.println("packet type: " + packet.getClass().getSimpleName());
+                        localsess.send(packet);
+                        
+                    }
+                });
+                
+                
+                
             }
         };
         proxy.addListener(proxlissten);
-       // proxy.addSession(clisession);
-        
-        // Add event listeners
-
-        // Connect to the server
-       
         proxy.bind();
     }
 }
